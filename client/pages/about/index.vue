a62c2c4aa2207681ac478061f4eaf047c9faa819
Peter Anderson <pete.anderson@gmail.com>
2019-01-29 20:45:43 +0000

    Started about page

--- client/pages/about/index.vue
+++ client/pages/about/index.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="about h-screen">
-        <h1>More about me</h1>
+    <div class="about flex h-screen">
+        <div>
+            <h1 class="">More about me</h1>
+            <p class="pt-3">I am a web developer based in Berlin which a special focus on Javascript. Currently love playing with Vue, Nuxt.js, React and React-Native</p>
+        </div>
+
     </div>
 </template>
 
@@ -8,7 +12,19 @@
 import navbar from "~/components/navbar";
 
 export default {
-  components: { navbar }
+  components: { navbar },
+  head() {
+      return {
+          title: "About Peter Anderson: I am a web developer based in Berlin which a special focus on Javascript. Currently love playing with Vue, Nuxt, React and React-Native",
+          meta: [
+              {
+                  hid: "About Peter Anderson: I am a web developer based in Berlin which a special focus on Javascript. Currently love playing with Vue, Nuxt, React and React-Native",
+                  name: "About Peter Anderson: I am a web developer based in Berlin which a special focus on Javascript. Currently love playing with Vue, Nuxt, React and React-Native",
+                  content: `About Peter Anderson: I am a web developer based in Berlin which a special focus on Javascript. Currently love playing with Vue, Nuxt, React and React-Native.`
+              }
+          ]
+      };
+  }
 };
 </script>
 
